df8e8ddd3296aa9b0ac3ff5297f598890443651d
netwareboss <netwareboss@gmail.com>
2014-07-17 13:10:19 +0800

    b

--- hello.c
+++ hello.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 void main()
 {
-  printf("hello world\n");
+  printf("hello world\n%d",is_odd(10));
 }
-
+int is_odd(int n)
+{
+ if (n==0)
+     return 1;
+ else 
+    return is_even(n-2);
+}
+int is_even(int n)
+{
+  if(n==1)
+    return 0;
+  else 
+    return is_odd(n-2);
+}
